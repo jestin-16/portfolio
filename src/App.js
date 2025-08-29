4b508d18dd2a2cd87207ef55fafa2c6ab2129a42
jestin16 <jestinshaji777@gmail.com>
2025-08-29 10:53:55 +0530

    currently under working status

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
        <h2>Software Developer</h2>
       </header>
+      <p>currently under development .Thanks for checking out my portfolio.
+        I'm a software developer with a passion for building web applications.
+        I'm a software developer with a passion for building web applications.</p>
     </div>
   );
 }
