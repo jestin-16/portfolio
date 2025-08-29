7d17dd9f3ee2e590e644c8315f69884301e6c54f
jestin16 <jestinshaji777@gmail.com>
2025-08-29 10:58:12 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h2>Software Developer</h2>
-      </header>
+      
       <p>currently under development .Thanks for checking out my portfolio.
         I'm a software developer with a passion for building web applications.
-        I'm a software developer with a passion for building web applications.</p>
+       </p>
+       </header>
     </div>
   );
 }
