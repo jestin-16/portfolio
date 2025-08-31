6f8946f8efe04aa1011f6850d0fc2e1a1dd05943
jestin16 <jestinshaji777@gmail.com>
2025-08-31 19:56:05 +0530

    working with tailwind css now on

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h2>Software Developer</h2>
+    // <div className="App">
+    //   <header className="App-header">
+    //    <h2>Software Developer</h2>
       
-      <p>currently under development .Thanks for checking out my portfolio.
-        I'm a software developer with a passion for building web applications.
-       </p>
-       </header>
-       <section>
-        <h3>About Me</h3>
-       </section>
+    //   <p className='text-2xl text-red-500'>currently under development .Thanks for checking out my portfolio.
+    //     I'm a software developer with a passion for building web applications.
+    //    </p>
+    //    </header>
+    //    <section>
+    //     <h3>About Me</h3>
+    //    </section>
+    // </div>
+    <div className=" bg-blue-500 h-screen App">
+      <header className="App-header">
+        <h2 className="text-2xl text-grey-800 text-bold">Software Developer</h2>
+      </header>
     </div>
   );
 }
