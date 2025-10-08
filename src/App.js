50534a30e1f54b694e24b9d48109522955920e21
jestin16 <jestinshaji777@gmail.com>
2025-10-08 21:54:20 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 // import './App.css';
+import jes from './images/jes.jpg';
 
 function App() {
   return (
     <>
   
   <header className="w-full h-20 bg-blue-200">
-<div className='flex justify-between items-center px-4 h-full'>
+<div className='flex items-center justify-between h-full px-4'>
   <div className="text-black font-fold">Software Developer</div>
   <nav>
     <ul className="flex space-x-4">
@@ -17,11 +18,14 @@ function App() {
   </nav>
 </div>
     </header>
+    <div className="w-32 h-32 ">
+      <img src={jes} alt="jestin"  className="object-cover w-32 h-32 rounded-full"/>
+    </div>
 
-    <div  className="h-screen bg-black">
-      <div className="flex flex-col justify-center items-center h-full">
-        <p className="text-4xl text-white">Hello, I'm Jestin </p>
-        <p className="text-4xl text-white">currently under development .Thanks for checking out my portfolio.
+  <div  className="h-screen bg-gray-50">
+      <div className="flex flex-col items-center justify-center h-full">
+        <p className="text-4xl ">Hello, I'm Jestin </p>
+        <p className="text-4xl ">currently under development .Thanks for checking out my portfolio.
         I'm a software developer with a passion for building web applications. </p>
         
       </div>
